ff74fd98b88578085fdf9dcaf9cc0c03c67b142f
HamzaT1992 <tsi.hamza1992@gmail.com>
2019-05-22 13:36:39 +0000

    added progress bar type 1

--- Extraction list Dossiers/Program.cs
+++ Extraction list Dossiers/Program.cs
@@ -14,8 +14,10 @@ namespace Extraction_list_Dossiers
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.GetEncoding(1252);
             // get application full path
             //string currentPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+
             if (args.Length == 0 || args.Length > 1)
             {
                 Console.WriteLine("Utiliser 1 argument le path");
@@ -64,17 +66,19 @@ namespace Extraction_list_Dossiers
             xlWorkSheet.Cells[1, 1] = "Dossier";
             xlWorkSheet.Cells[1, 2] = "Indice";
             int row = 2;
+            Console.WriteLine("Creation du fichier Excel...");
             foreach (var dos_ind in doss_inds)
             {
                 xlWorkSheet.Cells[row, 1] = dos_ind[0];
                 xlWorkSheet.Cells[row, 2] = dos_ind[1];
+                drawTextProgressBar(row - 2, doss_inds.Count());
                 row++;
             }
             
 
             xlWorkBook.SaveAs(path, XlFileFormat.xlOpenXMLWorkbook, misValue, misValue, misValue, misValue,
                 XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
-
+            Console.WriteLine("Enregistrement...");
             xlWorkBook.Close();
             xlApp.Quit();
 
@@ -84,8 +88,44 @@ namespace Extraction_list_Dossiers
 
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Excel file created successfully...");
+            Console.WriteLine("Open file " + path);
             Console.ForegroundColor = ConsoleColor.White;
             Process.Start(path);
         }
+
+        private static void drawTextProgressBar(int progress, int total)
+        {
+            //draw empty progress bar
+            Console.CursorLeft = 0;
+            Console.Write("["); //start
+            Console.CursorLeft = 32;
+            Console.Write("]"); //end
+            Console.CursorLeft = 1;
+            float onechunk = 30.0f / total;
+
+            //draw filled part
+            int position = 1;
+            for (int i = 0; i < onechunk * progress; i++)
+            {
+                Console.BackgroundColor = ConsoleColor.Gray;
+                Console.CursorLeft = position++;
+                Console.Write(" ");
+            }
+
+            //draw unfilled part
+            for (int i = position; i <= 31 ; i++)
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.CursorLeft = position++;
+                Console.Write(" ");
+            }
+
+            //draw totals
+            Console.CursorLeft = 35;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Write(progress.ToString() + " of " + total.ToString() + "    "); //blanks at the end remove any excess
+        }
     }
+
+    
 }
